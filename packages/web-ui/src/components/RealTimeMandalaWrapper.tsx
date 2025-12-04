2e80310102664d566313f4e8615d772c10bfa056
Mohamed Abdelaziz <amrikyy@gmail.com>
2025-12-04 09:34:53 +0200

    refactor: restructure project to Quantum Standard monorepo

--- packages/web-ui/src/components/RealTimeMandalaWrapper.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import React from 'react';
-import AxiomHolographicMandala from './AxiomHolographicMandala';
-
-export default function RealTimeMandalaWrapper() {
-  return <AxiomHolographicMandala />;
-}
